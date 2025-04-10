08047df89796bbe0d846dcc6aa173092e0d0e08c
Proky0 <astril79@gmail.com>
2025-04-10 04:57:23 +0200

    lint: add description

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,19 @@ import HomeView from '@/views/HomeView.vue';
 import ProjectsView from '@/views/ProjectsView.vue';
 import ProjectDetail from '@/views/ProjectDetail.vue'; // New component
 import ContactView from '@/views/ContactView.vue';
+/* Home View */
+import HomeView from '@views/HomeView.vue';
+
+/* Projects View */
+import ProjectsView from '@views/Project/ProjectsView.vue';
+import ProjectDetail from '@views/Project/ProjectDetail.vue';
+
+/* Contact View */
+import ContactView from '@views/ContactView.vue';
+
+/* Scripts View */
+import ScriptsView from '@views/Script/ScriptsView.vue';
+
 
 const routes = [
   { path: '/', component: HomeView },
